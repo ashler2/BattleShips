01807ff3cdef92257fdcceaa4c25012b7e20519b
janders4 <johnanderson198689@gmail.com>
2019-07-02 14:51:42 +0100

    grid uncover and score done

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-let grid = document.getElementsByClassName("container");
+let grid = [...document.getElementById("container").children];
+let score = document.getElementById("score");
+let guess = document.getElementById("guess");
+let scoreCount = 25;
 
-console.log(grid, "hello");
+grid.forEach(box => {
+  box.addEventListener("click", gridUncover);
+  box.addEventListener("click", scoreChange);
+});
+
+function gridUncover(id) {
+  id.target.style.background = "none";
+  id.target.style.border = "none";
+  id.target.className = "uncovered";
+}
+
+function scoreChange(id) {
+  id.target.removeEventListener("click", scoreChange);
+  score.innerText = `Score: ${scoreCount - 1}`;
+  scoreCount--;
+}
